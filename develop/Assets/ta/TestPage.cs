8ca483012ef4ad7b0018252488263989c776ac1b
vic <finalsteve@hotmail.com>
2014-06-08 23:10:10 +0800

    vic init

---
+++ develop/Assets/ta/TestPage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPage : MonoBehaviour {
+
+	public GameObject txt_showname;
+	void onTouchConsumerEventMouseDown( string eventName ){
+		txt_showname.GetComponent<TextMesh> ().text = eventName;
+	}
+
+	void onTouchConsumerEventMouseUp(){
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
